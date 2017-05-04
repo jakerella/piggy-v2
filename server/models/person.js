4f3140c1577d41c04a2783b4bf9efa69fe538b9a
Jordan Kasper <jordan@jordankasper.com>
2017-05-04 14:51:13 -0400

    Made an account on person creation

--- server/models/person.js
+++ server/models/person.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Person) {
 
+    Person.on('dataSourceAttached', function(obj){
+        var create = Person.create;
+        Person.create = function(data, context, cb) {
+            let that = this;
+            let account;
+
+            Person.app.models.Account.create({})
+                .then(function(record) {
+                    account = record;
+                    data.accountId = record.id;
+                    return create.apply(that, [data]);
+                })
+                .then(function(person) {
+                    person.account = account;
+                    console.log('new person created:', person);
+                    cb(null, person);
+                })
+                .catch(cb);
+        };
+    });
+
 };
