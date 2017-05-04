714836679b95b5be713ea860fa2b3eff997be3c2
Jordan Kasper <jordan@jordankasper.com>
2017-05-04 16:24:44 -0400

    Added ability to tie transactions to account and refactored code in that model

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -2,37 +2,72 @@
 
 module.exports = function(Transaction) {
 
-    Transaction.on('dataSourceAttached', function(obj){
+    Transaction.on('dataSourceAttached', function() {
+        setupFindFilter();
+        setupAccountAttachment();
+    });
+
+    function findAccountIdForPerson(id) {
+        return Transaction.app.models.Person.findById(id)
+            .then(function(person) {
+                if (!person) {
+                    let err = new Error('Unable to locate user account');
+                    err.status = 403;
+                    throw err;
+                } else if (!person.accountId) {
+                    console.error('No account attached to person:', id);
+                    let err = new Error('No account for user');
+                    err.status = 403;
+                    throw err;
+                }
+                return person.accountId;
+            });
+    }
+
+    function setupFindFilter() {
         var find = Transaction.find;
         Transaction.find = function(filter, context, cb) {
+            let that = this;
+
             if (!context || !context.accessToken) {
                 let err = new Error('You must be logged in to see transactions');
                 err.status = 401;
                 return cb(err);
             }
-            
-            Transaction.app.models.Person.findById(context.accessToken.userId, function(err, person) {
-                if (err) {
-                    return cb(err);
-                } else if (!person) {
-                    let err = new Error('Unable to locate user account');
-                    err.status = 403;
-                    return cb(err);
-                } else if (!person.accountId) {
-                    console.error('No account attached to person:', context.accessToken.userId);
-                    return cb(null, []);
-                }
 
-                if (!filter) {
-                    filter = { where: { accountId: person.accountId } };
-                } else {
-                    filter.where = { and: [ { accountId: person.accountId }, filter.where ] };
-                }
-                console.log('added account filter:', filter);
+            findAccountIdForPerson(context.accessToken.userId)
+                .then(function(accountId) {
+                    if (!filter) {
+                        filter = { where: { accountId: accountId } };
+                    } else {
+                        filter.where = { and: [ { accountId: accountId }, filter.where ] };
+                    }
+                    console.log('added account filter:', JSON.stringify(filter));
 
-                return find.apply(this, [filter, context, cb]);
-            });
+                    find.apply(that, [filter, context, cb]);
+                })
+                .catch(cb);
         };
-    });
+    }
+
+    function setupAccountAttachment() {
+        var create = Transaction.create;
+        Transaction.create = function(data, context, cb) {
+            let that = this;
+
+            if (!context || !context.accessToken) {
+                let err = new Error('You must be logged in to create transactions');
+                err.status = 401;
+                return cb(err);
+            }
+
+            findAccountIdForPerson(context.accessToken.userId)
+                .then(function(accountId) {
+                    data.accountId = accountId;
+                    create.apply(that, [data, context, cb]);
+                })
+                .catch(cb);
+        };
+    }
 
 };
